446734c7db4a5848ee5a4fcc03536f5d01033243
LuPiran <lucas13piran@gmail.com>
2022-03-20 08:08:02 -0300

    att 1.0

--- jogo.js
+++ jogo.js
@@ -10,14 +10,21 @@ function ajustaTamanhoPalcoJogo(){
 ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica(){
-    var posicaoX = Math.floor(Math.random() * largura)
-    var posicaoY = Math.floor(Math.random() * altura)
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
     console.log(posicaoX, posicaoY)
 
     //criar o elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
 }
