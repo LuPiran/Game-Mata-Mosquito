1ee9154d5d2bf8ecd66b8389e670eafa206bf221
LuPiran <lucas13piran@gmail.com>
2022-03-20 10:14:47 -0300

    att 1.3.0

--- jogo.js
+++ jogo.js
@@ -11,6 +11,13 @@ ajustaTamanhoPalcoJogo()
 
 //função para fazer o mosquito aparecer na tela randomicamente 
 function posicaoRandomica(){
+
+    //Removendo mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
+    
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -26,6 +33,7 @@ function posicaoRandomica(){
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
 
     document.body.appendChild(mosquito)
 
